2e49a573363a1f51cc955e3c9e15479e78b95776
Elderson Gama <elderson.gama@globalsys.com.br>
2024-09-11 10:50:44 -0300

    first commit

---
+++ frontend/build/precache-manifest.42606f2ddc462734b556535409891663.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb39943954a5f511b50f172869a5c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fea9fe18f6fb459cfc9",
+    "url": "/static/css/2.ef1e88a9.chunk.css"
+  },
+  {
+    "revision": "0b5c5be77f43d595c741",
+    "url": "/static/css/main.40de1c4b.chunk.css"
+  },
+  {
+    "revision": "0fea9fe18f6fb459cfc9",
+    "url": "/static/js/2.b835c507.chunk.js"
+  },
+  {
+    "revision": "b51ffe5e768838c0ce56a52763bf347e",
+    "url": "/static/js/2.b835c507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c54e1b03699fabca6b8",
+    "url": "/static/js/3.9b992bdc.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.9b992bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5c5be77f43d595c741",
+    "url": "/static/js/main.8901aa69.chunk.js"
+  },
+  {
+    "revision": "be58c27e3ef5db8a6f84",
+    "url": "/static/js/runtime-main.89282153.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "2c2b95e08697576394e8c8da5a7c7731",
+    "url": "/static/media/wa-background-dark.2c2b95e0.jpg"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background-light.adbccee0.png"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
